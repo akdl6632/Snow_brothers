cf6b4ddaf6f3c571edc2f0df86c98aad6ce0ce01
akdl6632 <58390849+akdl6632@users.noreply.github.com>
2022-11-26 00:26:43 +0900

    몹 2종류 추가

--- snow_brothers_code/enimies.py
+++ snow_brothers_code/enimies.py
@@ -18,6 +18,7 @@ FRAMES_PER_ACTION = 3
 class RedDemon:
     def __init__(self):
         # self.x, self.y = 640, 100 #825
+        # self.x, self.y = x, y #825
         self.x, self.y = random.randint(325, 960) , 100 #825
         self.frame = 0
         self.dir, self.face_dir = 1, 1
@@ -63,9 +64,119 @@ class RedDemon:
             game_world.remove_object(self)
             play_state.Nick_kill += 1
 
-            if play_state.Nick_kill == 10:
+            if play_state.Nick_kill == play_state.All_enimes[play_state.stage]:
                 play_state.stage += 1
                 print(f'stage is {play_state.stage}')
-                map.map_y += 224 * 9
+                map.map_y += 224
+                game_framework.change_state(play_state)
+    def get_bb(self):
+        return self.x - 30, self.y - 33, self.x + 30, self.y + 33
+
+class Frog:
+    def __init__(self):
+        # self.x, self.y = 640, 100 #825
+        self.x, self.y = random.randint(325, 960) , 100 #825
+        self.frame = 0
+        self.dir, self.face_dir = 1, 1
+        self.ver = 0
+        self.jump = 0
+        self.attack = 0
+        self.image = load_image('Enemies.png')
+        self.hp = 1
+
+    def update(self):
+        # self.frame = (self.frame + 1) % 2
+        # self.x += self.dir * 1
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 2
+        self.x += self.dir * RedDemon_RUN_SPEED_PPS * game_framework.frame_time
+        if self.x > 960:
+            self.x = 960
+            self.dir = -1
+            self.face_dir = -1
+        elif self.x < 325:
+            self.x = 325
+            self.dir = 1
+            self.face_dir = 1
+        # self.x = clamp(325, self.x, 960)
+        # self.y += self.ver * 1
+        # self.y = clamp(100, self.y, MAP_HEIGHT * MAP_SIZE)
+
+    def draw(self):
+        if self.dir == -1:
+            self.image.clip_draw(29 + int(self.frame) * 17, 469, 16, 26, self.x, self.y, 16 * 2.5, 26 * 2.5)  # 왼쪽 이동
+        elif self.dir == 1:
+            self.image.clip_composite_draw(29 + int(self.frame) * 17, 469, 16, 26, -3.141592, 'v', self.x, self.y, 16 * 2.5, 26 * 2.5)  # 오른쪽 이동
+        else:
+            # self.image.clip_draw IDLE
+            if self.face_dir == 1:
+                self.image.clip_composite_draw(1, 469, 23, 26, -3.141592, 'v', self.x, self.y, 24 * 2.5, 26 * 2.5)
+            else:
+                self.image.clip_draw(1, 469, 23, 26, self.x, self.y, 24 * 2.5, 26 * 2.5)
+
+        draw_rectangle(*self.get_bb())
+
+    def handle_collision(self, other, group):
+        if group == 'attack:enimies':
+            game_world.remove_object(self)
+            play_state.Nick_kill += 1
+
+            if play_state.Nick_kill == play_state.All_enimes[play_state.stage]:
+                play_state.stage += 1
+                print(f'stage is {play_state.stage}')
+                map.map_y += 224
+                game_framework.change_state(play_state)
     def get_bb(self):
         return self.x - 19, self.y - 33, self.x + 19, self.y + 33
+
+class Yellow_Troll:
+    def __init__(self):
+        self.x, self.y = random.randint(325, 960) , 100 #825
+        self.frame = 0
+        self.dir, self.face_dir = 1, 1
+        self.ver = 0
+        self.jump = 0
+        self.attack = 0
+        self.image = load_image('Enemies.png')
+        self.hp = 1
+
+    def update(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 2
+        self.x += self.dir * RedDemon_RUN_SPEED_PPS * game_framework.frame_time
+        if self.x > 960:
+            self.x = 960
+            self.dir = -1
+            self.face_dir = -1
+        elif self.x < 325:
+            self.x = 325
+            self.dir = 1
+            self.face_dir = 1
+        # self.x = clamp(325, self.x, 960)
+        # self.y += self.ver * 1
+        # self.y = clamp(100, self.y, MAP_HEIGHT * MAP_SIZE)
+
+    def draw(self):
+        if self.dir == -1:
+            self.image.clip_draw(29 + int(self.frame) * 25, 415, 24, 26, self.x, self.y, 24 * 2.5, 26 * 2.5)  # 왼쪽 이동
+        elif self.dir == 1:
+            self.image.clip_composite_draw(29 + int(self.frame) * 25, 415, 24, 26, -3.141592, 'v', self.x, self.y, 24 * 2.5, 26 * 2.5)  # 오른쪽 이동
+        else:
+            # self.image.clip_draw IDLE
+            if self.face_dir == 1:
+                self.image.clip_composite_draw(1, 415, 24, 26, -3.141592, 'v', self.x, self.y, 24 * 2.5, 26 * 2.5)
+            else:
+                self.image.clip_draw(1, 415, 24, 26, self.x, self.y, 24 * 2.5, 26 * 2.5)
+
+        draw_rectangle(*self.get_bb())
+
+    def handle_collision(self, other, group):
+        if group == 'attack:enimies':
+            game_world.remove_object(self)
+            play_state.Nick_kill += 1
+
+            if play_state.Nick_kill == play_state.All_enimes[play_state.stage]:
+                play_state.stage += 1
+                print(f'stage is {play_state.stage}')
+                map.map_y += 224
+                game_framework.change_state(play_state)
+    def get_bb(self):
+        return self.x - 30, self.y - 33, self.x + 30, self.y + 33
